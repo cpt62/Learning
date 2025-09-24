dc767bace103bbc1dee4a3b3e70ce6b38ccfe734
Alexey O. <rgrturzn7316@gmail.com>
2025-09-24 21:44:01 +0300

    Решение задачи с использованием интерфейса Supplier

---
+++ src/main/java/stepik/lambda/supplier/part1/Main.java
@@ -0,0 +1,34 @@
+package stepik.lambda.supplier.part1;
+
+import java.util.function.Supplier;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringSupplier ss = new StringSupplier(input);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ss.get());
+        }
+    }
+}
+
+class StringSupplier implements Supplier<String> {
+    private String[] array;
+    private static int currentIndex;
+
+    public StringSupplier(String input) {
+        array = input.split(" +");
+        currentIndex = 0;
+    }
+
+    @Override
+    public String get() {
+        if (currentIndex < array.length) {
+            return array[currentIndex++];
+        } else {
+            return null;
+        }
+    }
+}
